1cd9a2d29aea20f393bdba1d71414b2d0de6e1ac
techninja <james@tn42.com>
2014-03-22 15:23:47 -0700

    Simplify server input to accept anything and send it on return (with linefeed!)

--- simple_server.js
+++ simple_server.js
@@ -25,17 +25,8 @@ var server = net.createServer(function(socket){
   console.log("Someone connected from " + socket.remoteAddress + ":" + socket.remotePort + "!");
   process.stdout.write('>> ');
   process.stdin.on('data', function(d) {
-    d = d.toString('utf8', 0, d.length - 1);
-    if (/^[0-7][lh]$/i.test(d)) {
-      socket.write(d.toLowerCase());
-    } else if ('x' === d) {
-      process.exit(0);
-    } else {
-      console.log("Commands: 0h  Set pin D0 high");
-      console.log("          7l  Set pin D7 low");
-      console.log("              Any pin 0-7 may be set high or low");
-      console.log("          x   Exit");
-    }
+    d = d.toString('utf8', 0, d.length);
+    socket.write(d);
     process.stdout.write('>> ');
   });
 });
