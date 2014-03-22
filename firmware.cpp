10480a147d70a25c2c37f2d442216090e0a00f51
techninja <james@tn42.com>
2014-03-22 15:28:00 -0700

    Add some documentation and parse port and speed from API func

--- firmware.cpp
+++ firmware.cpp
@@ -6,6 +6,7 @@ int led = 7;
 long speed = 9600;
 bool serialSetup = false;
 
+// Convert IP string into byte array
 void ipArrayFromString(byte ipArray[], String ipString) {
   int dot1 = ipString.indexOf('.');
   ipArray[0] = ipString.substring(0, dot1).toInt();
@@ -16,16 +17,24 @@ void ipArrayFromString(byte ipArray[], String ipString) {
   ipArray[3] = ipString.substring(dot1 + 1).toInt();
 }
 
-int connectToMyServer(String ip) {
+// Spark API function handler to connect this core to a local server
+// 'connect' format: [IPv4]:[PORT]@[BAUD] -- EG 192.168.0.42:9025@9600
+int connectToMyServer(String connect) {
+  String ip = connect.substring(0, connect.indexOf(':'));
   byte serverAddress[4];
   ipArrayFromString(serverAddress, ip);
 
-  if (client.connect(serverAddress, 9000)) {
+  int port = connect.substring(connect.indexOf(':') + 1, connect.indexOf('@')).toInt();
+  speed = (long) connect.substring(connect.indexOf('@') + 1).toInt();
+
+
+  if (client.connect(serverAddress, port)) {
     return 1; // successfully connected
   } else {
     return -1; // failed to connect
   }
 }
+
 
 void setup() {
   Spark.function("connect", connectToMyServer);
